b2851ef32c997967eade565d9cdeb09ac9993945
Rowan Manning <accounts+github@rowanmanning.com>
2019-02-18 09:50:08 +0000

    Ensure that competency IDs are unique

--- script/validate-competencies-json.js
+++ script/validate-competencies-json.js
@@ -8,7 +8,7 @@ const schema = require('../test/schema/schema.json');
 outputValidationErrors();
 
 function outputValidationErrors() {
-	const errors = getValidationErrors();
+	const errors = [...getValidationErrors(), ...getIdErrors()];
 	if (!errors.length) {
 		return console.log('No validation errors found in the competencies');
 	}
@@ -26,6 +26,25 @@ function getValidationErrors() {
 	return (validate(competencies) ? [] : validate.errors);
 }
 
+function getIdErrors() {
+	const errors = [];
+	if (Array.isArray(competencies)) {
+		const ids = competencies
+			.filter(competency => typeof competency === 'object' && competency !== null)
+			.map(competency => competency.id)
+			.filter(competency => Boolean(competency));
+		const duplicates = findDuplicates(ids);
+		if (duplicates.length) {
+			errors.push({
+				dataPath: '',
+				message: 'should contain only unique IDs',
+				duplicateIds: duplicates
+			})
+		}
+	}
+	return errors;
+}
+
 function errorToString(error) {
 	const value = pointer(competencies, error.dataPath);
 	let expected = error.message;
@@ -33,8 +52,28 @@ function errorToString(error) {
 		expected += ` (${error.params.allowedValues.join(', ')})`;
 	}
 	const valueAsJson = JSON.stringify(value, null, '\t');
+	let received = valueAsJson;
+	if (error && error.params && error.params.additionalProperty) {
+		received = `additional property "${error.params.additionalProperty}"`;
+	}
+	if (error && error.duplicateIds) {
+		received = `duplicate IDs ${error.duplicateIds.join(', ')}`;
+	}
 	return `Error in value at ${error.dataPath}:
 		├── Expected: ${expected}
-		└── Received: ${valueAsJson}
+		└── Received: ${received}
 	`.trim().replace(/\t/g, '');
+}
+
+function findDuplicates(array) {
+	const found = [];
+	const duplicates = [];
+	for (const item of array) {
+		if (!found.includes(item)) {
+			found.push(item);
+		} else if (!duplicates.includes(item)) {
+			duplicates.push(item);
+		}
+	}
+	return duplicates;
 }
